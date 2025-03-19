93fde4f73a8bbf13adc0c862047da526507e9468
Sneh Doshi <doshisneh75@gmail.com>
2025-03-19 21:39:19 +0530

    Save Cart in Local Storage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,9 +1,8 @@
-   import { cart , addToCart } from "../data/cart.js";
-  import { products } from "../data/products.js";
-  import { formatCurrency } from './utils/money.js';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-
-  let productsHTML = '';
+let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
@@ -59,8 +58,9 @@ products.forEach((product) => {
   `;
 });
 
-function updateCartQuantity(){
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
   let cartQuantity = 0;
 
   cart.forEach((cartItem) => {
@@ -71,16 +71,11 @@ function updateCartQuantity(){
     .innerHTML = cartQuantity;
 }
 
-document.querySelector('.js-product-grid').innerHTML = productsHTML;
-
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset;
+      const productId = button.dataset.productId;
       addToCart(productId);
       updateCartQuantity();
-
-      
     });
   });
-  
