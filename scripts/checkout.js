9f27a0e317824586a7233bcb39a29f675591e92c
Sneh Doshi <doshisneh75@gmail.com>
2025-03-28 14:37:00 +0530

    Create OOP version of  cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import {renderOrderSummary} from './checkout/ordersummary.js';
 import {renderPaymentSummary} from './checkout/paymentsummary.js';
+import '../data/cart-oop.js';
 renderPaymentSummary();
 renderOrderSummary();
