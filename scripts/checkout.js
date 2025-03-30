02532cc6bcfa149465598b03c817f76e4109ab17
Sneh Doshi <doshisneh75@gmail.com>
2025-03-30 15:26:02 +0530

    Load Products From the Backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
 import {renderOrderSummary} from './checkout/ordersummary.js';
 import {renderPaymentSummary} from './checkout/paymentsummary.js';
+import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
 import '../data/backend-practice.js';
-renderPaymentSummary();
-renderOrderSummary();
+loadProducts(() => {
+  renderPaymentSummary();
+  renderOrderSummary();
+});
